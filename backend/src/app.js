0c4d899a0659470a13044f82a33f5d47a78b241d
Louis F <72155852+itsmelouis@users.noreply.github.com>
2024-07-01 11:04:40 +0200

    feat: add cors to api

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,7 @@
 import express from 'express';
+import cors from 'cors';
 import authRouter from './routes/auth/authRouter.js';
+import userRouter from './routes/user/userRouter.js';
 
 const app = express();
 const port = 3000
@@ -7,9 +9,11 @@ const port = 3000
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+app.use(cors());
 
 app.use('/auth', authRouter);
+app.use('/user', userRouter);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`L'API tourne sur http://localhost:${port}`)
 })
